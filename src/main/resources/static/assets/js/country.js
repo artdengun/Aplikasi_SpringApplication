c4fc9064141557c6a7237512b362908dc1c87fdd
Deni Gunawan <artdengun@gmail.com>
2020-10-09 00:47:03 +0700

    test

--- src/main/resources/static/assets/js/country.js
+++ src/main/resources/static/assets/js/country.js
@@ -1,43 +1,9 @@
 
 $('document').ready(function() {
 
+	$('.label #editButton').on('click',function(event){
 
+		$('#editModal').modal();
 
-	$('table #editButton').on('click',function(event){
-//		event.preventDefault();
-//
-//		var href= $(this).attr('href');
-//
-//		$.get(href, function(country, status){
-//			$('#idEdit').val(country.id);
-//			$('#capitalEdit').val(country.capital);
-//			$('#codeEdit').val(country.code);
-//			$('#continentEdit').val(country.continent);
-//			$('#descriptionEdit').val(country.description);
-//			$('#nationalityEdit').val(country.nationality );
-//		});
-		$('#editModal').modal();		
 	});
-
 });
-//
-//	$('.table #detailsButton').on('click',function(event) {
-//		event.preventDefault();
-//		var href= $(this).attr('href');
-//		$.get(href, function(country, status){
-//			$('#idDetails').val(country.id);
-//			$('#descriptionDetails').val(country.description);
-//			$('#codeDetails').val(country.code);
-//			$('#lastModifiedByDetails').val(country.lastModifiedBy);
-//			$('#lastModifiedDateDetails').val(country.lastModifiedDate.substr(0,19).replace("T", " "));
-//		});
-//		$('#detailsModal').modal();
-//	});
-//
-//	$('.table #deleteButton').on('click',function(event) {
-//		event.preventDefault();
-//		var href = $(this).attr('href');
-//		$('#deleteModal #delRef').attr('href', href);
-//		$('#deleteModal').modal();
-//	});
-//});
